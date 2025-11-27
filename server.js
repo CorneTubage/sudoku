9713113d2d92b14b57461a317bd7fb6d91d77cda
CorneTubage <150783232+CorneTubage@users.noreply.github.com>
2025-11-27 15:25:48 +0100

    Modification of the server.js file for launching the project locally

--- server.js
+++ server.js
@@ -290,4 +290,4 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
+server.listen(PORT, () => console.log(`Serveur lancé sur le port http://127.0.0.1:${PORT}`));
